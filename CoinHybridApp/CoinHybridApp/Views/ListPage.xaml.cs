5d57a8a49b9c0fc1e51973e89837ac54da723950
Anthony Dole <anthony.dole.pro@gmail.com>
2020-04-20 11:43:41 +0200

    Add search bar for crypto

--- CoinHybridApp/CoinHybridApp/Views/ListPage.xaml.cs
+++ CoinHybridApp/CoinHybridApp/Views/ListPage.xaml.cs
@@ -36,5 +36,18 @@ namespace CoinHybridApp
              await Navigation.PushAsync(new ListPageDetail(mydetails.Name, mydetails.PriceUsd, mydetails.Symbol,mydetails.ChangePercent24Hr,mydetails.VolumeUsd24Hr,mydetails.MarketCapUsd,mydetails.Supply,mydetails.ImageUrl));
 
             }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var _container = BindingContext as ListPageViewModel;
+            CryptoListView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                CryptoListView.ItemsSource = _container.Cryptos;
+            else
+                CryptoListView.ItemsSource = _container.Cryptos.Where(i => i.Name.Contains(e.NewTextValue));
+
+            CryptoListView.EndRefresh();
+        }
     }
 }
