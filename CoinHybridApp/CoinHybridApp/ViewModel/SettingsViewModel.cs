d2e9fc4835f8abaa1abadaf5dc49b5ed95a040c1
Anthony Dole <anthony.dole.pro@gmail.com>
2020-04-21 13:43:51 +0200

    add settings view model

---
+++ CoinHybridApp/CoinHybridApp/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CoinHybridApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CoinHybridApp.ViewModel
+{
+   public class SettingsViewModel : BaseViewModel
+    {
+     
+    }
+}
