486cc0bb29d5a01cce2fbc51d6281612c14cac8a
Vuillaume.Adrien <adrien.vuillaume@etu.univ-cotedazur.fr>
2020-04-22 15:05:37 +0200

    CreateUser is now working, user is created in the database.

--- CoinHybridApp/CoinHybridApp/Views/CreateUser.xaml.cs
+++ CoinHybridApp/CoinHybridApp/Views/CreateUser.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoinHybridApp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,20 @@ namespace CoinHybridApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreateUser : ContentPage
     {
+
+        CreateUserViewModel VM;
         public CreateUser()
         {
             InitializeComponent();
             Title = "Créer un profil";
+            VM = new CreateUserViewModel();
+            this.BindingContext = VM;
         }
 
         private void createUser(object sender, EventArgs e)
         {
+            VM.newUser();
+            Navigation.PopAsync();
         }
     }
 }
