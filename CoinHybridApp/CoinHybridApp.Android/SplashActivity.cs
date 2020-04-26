e8fa517ce3e401fc1d3194f34d57fbee67755199
Anthony Dole <anthony.dole.pro@gmail.com>
2020-04-26 15:19:47 +0200

    Fix design color and splash screen problem

--- CoinHybridApp/CoinHybridApp.Android/SplashActivity.cs
+++ CoinHybridApp/CoinHybridApp.Android/SplashActivity.cs
@@ -14,13 +14,19 @@ using Android.Widget;
 namespace CoinHybridApp.Droid
 {
     [Activity(Label ="CoinHybridApp", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class SplashActivity : Activity
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+       
             base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
-            
+            Finish();
 
             // Create your application here
         }
